e39fddac04cb059ef498d22863f001bb0a7082da
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-11 11:52:40 +0900

    update: classAndInstance.dart_클래스생성자공부

--- dart_practice/Part3/classAndInstance.dart
+++ dart_practice/Part3/classAndInstance.dart
@@ -1,7 +1,10 @@
 void main() {
   print('class 공부');
   // instance 인스턴스 : object (객체) 생성
-  Student girlStudent = Student();
+  // Student girlStudent = Student('taetae', 20);
+  // 생성자 예시를 위한 인스턴스 생성
+  Student girlStudent = Student(name: 'gugu', age: 21);
+
   print(girlStudent.name); // Kim
   print(girlStudent.age); // null
 
@@ -27,6 +30,19 @@ void main() {
 // - 비슷한 성격을 가진 연관있는 변수와 함수들을 한 class 에 정의합니다.
 
 class Student {
+  // constructor : 생성자
+  // 생성자의 매개변수에 this 를 사용해서 인스턴스의 파라미터로 받아 넣어줄 경우!
+  // Student(this.name, this.age);
+  // 생성자의 매개변수에 (타입 매개변수, 타입 매개변수) 넣을 경우, 잘못된 방법이다.
+  // Student(String name2, int age2) {
+  //   print('생성자 실행');
+  //   this.name = name2;
+  //   this.age = age2;
+  // }
+
+  // 생성자 에서 name 뒤에 학생을 붙이고 싶을때!, 그리고 생성자 이기떄문에 클래스가 생성될 떄 딱한번만 실행 된다.
+  Student({required String name, this.age}) : name = '$name 학생';
+
   String name = 'Kim';
   int? age;
 
