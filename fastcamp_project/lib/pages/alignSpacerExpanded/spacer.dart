b6ebe52ba1d8fe006b1abb21bf75629c856c9210
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-14 17:38:24 +0900

    feat: spacer.dart_공간제어위젯공부(Align, Spacer, Expanded)

---
+++ fastcamp_project/lib/pages/alignSpacerExpanded/spacer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SpacerPage extends StatelessWidget {
+  const SpacerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter'),
+      ),
+      body: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            color: Colors.yellow,
+            height: 40,
+            width: 40,
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          Container(
+            color: Colors.green,
+            height: 40,
+            width: 40,
+          ),
+          Container(
+            color: Colors.blue,
+            height: 40,
+            width: 40,
+          ),
+          const Spacer(),
+          Container(
+            color: Colors.red,
+            height: 40,
+            width: 40,
+          ),
+          Container(
+            color: Colors.purple,
+            height: 40,
+            width: 40,
+          ),
+        ],
+      ),
+    );
+  }
+}
