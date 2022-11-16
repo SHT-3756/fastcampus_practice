5e93a8ae2f408e4816cb627d05940e72bdbd9f92
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-16 17:07:49 +0900

    feat: navigator.dart_navigator 공부(navigator 로 페이지전환)

--- fastcamp_project/lib/pages/meterial/navigator.dart
+++ fastcamp_project/lib/pages/meterial/navigator.dart
@@ -6,10 +6,94 @@ class NaviGatorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text(
-        'hello',
-        style: Theme.of(context).textTheme.headline4,
+      appBar: AppBar(
+        title: const Text('hello Home'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'home page',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const NavigatorFirstPage()));
+              },
+              child: const Text('NextPage 로 이동'))
+        ],
+      ),
+    );
+  }
+}
+
+// firstPage
+class NavigatorFirstPage extends StatelessWidget {
+  const NavigatorFirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NavigatorFirstPage'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              'hello This is NavigatorFirstPage',
+            ),
+          ),
+          const Icon(Icons.ac_unit),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const NavigatorSecondPage()));
+              },
+              child: const Text('PrevPage 로 이동')),
+        ],
+      ),
+    );
+  }
+}
+
+// secondPage
+class NavigatorSecondPage extends StatelessWidget {
+  const NavigatorSecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NavigatorSecondPage'),
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'hello This is NavigatorSecondPage',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ],
       ),
     );
   }
