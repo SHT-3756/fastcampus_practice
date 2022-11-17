b6e5215520377ebd147cd3945af1e0f4605a5487
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-17 21:18:57 +0900

    feat: home_page.dart_버튼클릭 후 알림 호출기능 완료

--- study_package/lib/main.dart
+++ study_package/lib/main.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:study_package/page/home_page.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() {
+  // 플러터 엔진과 상호작용을 위해서 선언
+  WidgetsFlutterBinding.ensureInitialized();
+  _initNotiSetting();
   runApp(const MyApp());
+}
+// flutter_local_notifications 패키지 안에 FlutterLocalNotificationsPlugin의 생성자가 필요하기 때문에,import 해주자!
+// 그리고 외부에서 이 생성자를 접근 할 일이 있으니, _initNotiSetting() 안에 넣지 않았다.
+
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+// private 으로 작성된 이유는 이 파일 내부에서만 접근할 것이기 때문
+void _initNotiSetting() async {
+  const initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  // 메개변수를 보면 알듯이 6개의 매개변수가 defaultValue 가 true 이다. 변경하려면 이렇게 작성
+  // 앱이 처음 구동 되면 Permission 을 체크하는데, 지금은 앱이 추가된 시점에 물어보겠다라는 의미로 false
+  const initializationSettingsIOS = DarwinInitializationSettings(
+    // 이 포그라운드에 있는 동안 알림이 트리거되면 경고를 나타내는 기본 설정이 표시
+    requestAlertPermission: true,
+    // 앱이 포그라운드에 있는 동안 알림이 트리거될 때 배지 값을 적용해야 하는지 여부
+    requestBadgePermission: true,
+    // 앱이 포그라운드에 있을 때 알림이 트리거될 때 소리를 재생해야 하는지 여부
+    requestSoundPermission: true,
+  );
+
+  // ios, android 초기화 하는 함수들을 변수에 저장해두고  InitializationSettings 생성자에 보낸다.
+  const initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
 }
 
 class MyApp extends StatelessWidget {
--- study_package/lib/page/home_page.dart
+++ study_package/lib/page/home_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:study_package/main.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -39,6 +43,47 @@ class _HomePageState extends State<HomePage> {
                   EasyLoading.removeAllCallbacks();
                 },
                 child: const Text('btn')),
+            ElevatedButton(
+                onPressed: () async {
+                  // flutterLocalNotificationsPlugin 객체를 불러온다.
+                  final notification = flutterLocalNotificationsPlugin;
+                  // android AndroidNotificationDetails 객체 생성
+                  const android = AndroidNotificationDetails(
+                    '0',
+                    'android 알림 제목',
+                    channelDescription: 'android 알림 바디',
+                    importance: Importance.max,
+                    priority: Priority.max,
+                  );
+                  // ios DarwinNotificationDetails 객체 생성
+                  const ios = DarwinNotificationDetails();
+
+                  const detail = NotificationDetails(
+                    android: android,
+                    iOS: ios,
+                  );
+
+                  // 비동기로 작성한 이유는, requestPErmissions 의 결과값이 중요하기 때문
+                  // 안드로이드일때는 무조건 true 이고, ios 일때는 requestPermission 결과를 가져와야함
+                  // requestPermissions 가 nullable 한값이기 때문에 만약에 null 로 오면 false 로 값받는다.
+                  final permission = Platform.isAndroid
+                      ? true
+                      : await notification
+                              .resolvePlatformSpecificImplementation<
+                                  IOSFlutterLocalNotificationsPlugin>()
+                              ?.requestPermissions(
+                                  alert: true, badge: true, sound: true) ??
+                          false;
+                  if (!permission) {
+                    // 만약 permission 이 false 라면 알람을 띄워줄 수 없으니, 조건문 작성
+                    // await showNotiPermissionDialog(context);
+                    // return toast 권한이 없습니다.
+                    return;
+                  }
+                  await flutterLocalNotificationsPlugin.show(
+                      0, 'ios 알림 제목', 'ios 알림 내용', detail);
+                },
+                child: const Text('알림 버튼')),
             const Center(
               child: Text('hi'),
             )
