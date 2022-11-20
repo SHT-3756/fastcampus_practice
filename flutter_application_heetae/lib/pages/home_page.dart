ef840661b74217266e0d2168e7eaf3ee997cf46d
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-21 01:32:53 +0900

    feat: today, history.dart_따로 페이지 생성해서 import, Navigator.push 함수 분리, 하단앱바 클릭시 페이지 인덱스 하는것 분리

--- flutter_application_heetae/lib/pages/home_page.dart
+++ flutter_application_heetae/lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_heetae/components/custom_colors.dart';
 import 'package:flutter_application_heetae/pages/add/add_page.dart';
+import 'package:flutter_application_heetae/pages/history/history_page.dart';
+import 'package:flutter_application_heetae/pages/today/today_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,10 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
-  final _pages = [
-    Container(color: Colors.blue),
-    Container(color: Colors.green),
-  ];
+  final _pages = [const HistoryPage(), const TodayPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -29,52 +28,58 @@ class _HomePageState extends State<HomePage> {
           body: _pages[_currentIndex],
           floatingActionButton: FloatingActionButton(
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const AddPage(),
-                    ));
+                _onAddMedicine(context);
               },
               child: const Icon(CupertinoIcons.add)),
           floatingActionButtonLocation:
               FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: BottomAppBar(
-              elevation: 0,
-              child: Container(
-                height: kBottomNavigationBarHeight,
-                color: Colors.yellow,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    CupertinoButton(
-                        child: Icon(
-                          CupertinoIcons.checkmark,
-                          color: _currentIndex == 0
-                              ? CustomColors.primaryColor
-                              : Colors.grey[400],
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _currentIndex = 0;
-                          });
-                        }),
-                    CupertinoButton(
-                        child: Icon(
-                          CupertinoIcons.text_badge_checkmark,
-                          color: _currentIndex == 1
-                              ? CustomColors.primaryColor
-                              : Colors.grey[400],
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _currentIndex = 1;
-                          });
-                        })
-                  ],
-                ),
-              )),
+          bottomNavigationBar: _buildBottomAppBar(),
         ),
       ),
     );
+  }
+
+  BottomAppBar _buildBottomAppBar() {
+    return BottomAppBar(
+        elevation: 0,
+        child: Container(
+          height: kBottomNavigationBarHeight,
+          color: Colors.yellow,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CupertinoButton(
+                  child: Icon(
+                    CupertinoIcons.checkmark,
+                    color: _currentIndex == 0
+                        ? CustomColors.primaryColor
+                        : Colors.grey[400],
+                  ),
+                  onPressed: () => _onSelectPage(0)),
+              CupertinoButton(
+                  child: Icon(
+                    CupertinoIcons.text_badge_checkmark,
+                    color: _currentIndex == 1
+                        ? CustomColors.primaryColor
+                        : Colors.grey[400],
+                  ),
+                  onPressed: () => _onSelectPage(1))
+            ],
+          ),
+        ));
+  }
+
+  void _onSelectPage(int pageIndex) {
+    setState(() {
+      _currentIndex = pageIndex;
+    });
+  }
+
+  void _onAddMedicine(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const AddPage(),
+        ));
   }
 }
