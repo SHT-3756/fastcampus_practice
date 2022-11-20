17be469d37d3afe3c03ab719e881a89639cd947f
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-20 23:55:26 +0900

    feat: custem_theme.dart_커스텀라이트모드, 커스텀다크모드 getter 생성

--- flutter_application_heetae/lib/components/custom_theme.dart
+++ flutter_application_heetae/lib/components/custom_theme.dart
@@ -3,41 +3,45 @@ import 'package:flutter/material.dart';
 
 class CustomTheme {
   static ThemeData get lightTheme => ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: CustomColors.primaryMaterialColor,
         fontFamily: 'GmarketSansTTF',
+        scaffoldBackgroundColor: Colors.white,
+        splashColor: Colors.white,
         textTheme: _textTheme,
+        brightness: Brightness.light,
+      );
+  static ThemeData get darkTheme => ThemeData(
+        primarySwatch: CustomColors.primaryMaterialColor,
+        fontFamily: 'GmarketSansTTF',
+        splashColor: Colors.white,
+        textTheme: _textTheme,
+        brightness: Brightness.dark,
       );
 
   static const TextTheme _textTheme = TextTheme(
     headline4: TextStyle(
       fontSize: 32,
       fontWeight: FontWeight.w400,
-      color: CustomColors.primaryColor,
     ),
     subtitle1: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w500,
-      color: CustomColors.primaryColor,
     ),
     subtitle2: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w400,
-      color: CustomColors.primaryColor,
     ),
     bodyText1: TextStyle(
       fontSize: 15,
       fontWeight: FontWeight.w300,
-      color: CustomColors.primaryColor,
     ),
     bodyText2: TextStyle(
       fontSize: 14,
       fontWeight: FontWeight.w300,
-      color: CustomColors.primaryColor,
     ),
     button: TextStyle(
       fontSize: 12,
       fontWeight: FontWeight.w200,
-      color: CustomColors.primaryColor,
     ),
   );
 }
