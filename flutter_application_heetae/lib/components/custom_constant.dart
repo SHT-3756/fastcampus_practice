46b89bb4a5074c472b33d7a4ddd193acfc68f66c
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-21 15:58:37 +0900

    feat: add_medicine_page.dart_스타일 디테일 수정 및 고유값 상수 활용해서 적용하기, feat: custom_constant.dart_pagePadding, textFieldContentPadding, submitButtonBoxPadding 추가

--- flutter_application_heetae/lib/components/custom_constant.dart
+++ flutter_application_heetae/lib/components/custom_constant.dart
@@ -1,11 +1,28 @@
-// 20.0
+import 'package:flutter/material.dart';
+
+/// 20.0
 const double regularSpace = 20.0;
 
-// 40.0
+/// 40.0
 const double largeSpace = 45.0;
 
-// 10.0
+/// 10.0
 const double smallSpace = 10.0;
 
-// 48.0
+/// 48.0
 const double submitButtonHeight = 48.0;
+
+/// `all`
+/// 20.0
+const EdgeInsetsGeometry pagePadding = EdgeInsets.all(20.0);
+
+/// `symmetric`
+/// horizontal: 6
+const EdgeInsetsGeometry textFieldContentPadding =
+    EdgeInsets.symmetric(horizontal: 6);
+
+/// `symmetric`
+/// horizontal: 20
+/// vertical: 10
+const EdgeInsetsGeometry submitBUttonBoxPadding =
+    EdgeInsets.symmetric(horizontal: 20, vertical: 10);
--- flutter_application_heetae/lib/pages/add_medicine/add_medicine_page.dart
+++ flutter_application_heetae/lib/pages/add_medicine/add_medicine_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_heetae/components/custom_constant.dart';
 
-class AddMedicinePage extends StatelessWidget {
+class AddMedicinePage extends StatefulWidget {
   const AddMedicinePage({super.key});
+
+  @override
+  State<AddMedicinePage> createState() => _AddMedicinePageState();
+}
+
+class _AddMedicinePageState extends State<AddMedicinePage> {
+  final _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +32,11 @@ class AddMedicinePage extends StatelessWidget {
           FocusScope.of(context).unfocus();
         },
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: pagePadding,
           child: Column(
             // 반대축 정렬 start
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(
-                height: largeSpace,
-              ),
               Text(
                 '어떤 약인가요?',
                 style: Theme.of(context).textTheme.headline4,
@@ -54,19 +65,39 @@ class AddMedicinePage extends StatelessWidget {
                 '약 이름',
                 style: Theme.of(context).textTheme.subtitle1,
               ),
-              TextFormField(),
+              TextFormField(
+                controller: _nameController,
+                maxLength: 20,
+                // 키패트 타입
+                keyboardType: TextInputType.text,
+                // 키패드의 submit 버튼
+                textInputAction: TextInputAction.done,
+                style: Theme.of(context).textTheme.bodyText1,
+                // 글자를 입력할때 스타일
+                decoration: InputDecoration(
+                  hintText: '복용할 막 이름을 기입 해주세요.',
+                  hintStyle: Theme.of(context).textTheme.bodyText2,
+                  // 가로 여백 6만큼 적용
+                  contentPadding: textFieldContentPadding,
+                ),
+              ),
             ],
           ),
         ),
       ),
-      bottomNavigationBar:
-          // ios x 이상만 영향이 간다. 이전 버전은 아무 변화 없다.
-          SafeArea(
-              child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-            textStyle: Theme.of(context).textTheme.subtitle1),
-        child: Text('다음'),
+      // SafeArea : ios x 이상만 영향이 간다. 이전 버전은 아무 변화 없다.
+      bottomNavigationBar: SafeArea(
+          child: Padding(
+        padding: submitBUttonBoxPadding,
+        child: SizedBox(
+          height: submitButtonHeight,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.subtitle1),
+            child: Text('다음'),
+          ),
+        ),
       )),
     );
   }
