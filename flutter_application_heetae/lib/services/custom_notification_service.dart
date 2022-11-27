ca32a38313163a83fe8fc42943e11c2ed08b9898
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-27 15:26:06 +0900

    update: add_medicine_service.dart__alrms 객체 값 변경, custom_notification_service.dart_주석 추가

--- flutter_application_heetae/lib/services/custom_notification_service.dart
+++ flutter_application_heetae/lib/services/custom_notification_service.dart
@@ -42,13 +42,21 @@ class CustomNotificationService {
     required String title, // HH:mm 약 먹을 시간이예요!
     required String body, // {약이름} 복약했다고 알려주세요!
   }) async {
+    // permissionNotification 의 return 값이 false 이면은 return false
     if (!await permissionNotification) {
+      // sho native setting page
+      // 알람 권한이 없으면 설정창으로 돌아가서 다시 추가해줘
       return false;
     }
 
+    // 예외
     final now = tz.TZDateTime.now(tz.local);
     final alarmTime = DateFormat('HH:mm').parse(alarmTimeStr);
 
+    // 조건 1 세팅알람 시(hour)가 현재 시(hour) 보다 적은 경우 또는
+    // 조건 2 (세팅알람 시(hour)가 현재 시(hour)와 같은 경우 그리고 세팅알람 분(minute)이 현재 분(minute)와 같거나 작은 경우)
+    // 결과 true -> 현재 일(day) + 1일
+    // 결과 false -> 현재 일(day)
     final day = (alarmTime.hour < now.hour ||
             alarmTime.hour == now.hour && alarmTime.minute <= now.minute)
         ? now.day + 1
@@ -57,8 +65,9 @@ class CustomNotificationService {
     // id
     final alarmTimeId = alarmTimeStr.replaceAll(':', '');
 
+    // add schedule notification
     final details = _notificationDetails(
-      alarmTimeId,
+      alarmTimeId, // unique
       title: title,
       body: body,
     );
@@ -107,10 +116,11 @@ class CustomNotificationService {
   }
 
   Future<bool> get permissionNotification async {
+    // android 일때는 true 로 리턴
     if (Platform.isAndroid) {
       return true;
     }
-
+    // ios 일때는 권한을 묻는다. 만약에 권한이 없을경우 false 로 리턴
     if (Platform.isIOS) {
       return await notification
               .resolvePlatformSpecificImplementation<
@@ -118,7 +128,7 @@ class CustomNotificationService {
               ?.requestPermissions(alert: true, badge: true, sound: true) ??
           false;
     }
-
+    // android 와 ios 가 아닐경우 false 로 리턴
     return false;
   }
 }
