48d17fb0b945f82495ea2e803ded20d981f28998
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-30 17:36:03 +0900

    feat: today_page.dart, home_page.dart_ 목록창, 단순 UI 제작

--- flutter_application_heetae/lib/pages/home_page.dart
+++ flutter_application_heetae/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_heetae/components/custom_colors.dart';
+import 'package:flutter_application_heetae/components/custom_constant.dart';
 import 'package:flutter_application_heetae/pages/add_medicine/add_medicine_page.dart';
 import 'package:flutter_application_heetae/pages/history/history_page.dart';
 import 'package:flutter_application_heetae/pages/today/today_page.dart';
@@ -15,58 +16,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
-  final _pages = [const HistoryPage(), const TodayPage()];
+  final _pages = [
+    TodayPage(),
+    const HistoryPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
-      child: SafeArea(
-        top: false,
-        child: Scaffold(
-          appBar: AppBar(),
-          body: _pages[_currentIndex],
-          floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                _onAddMedicine(context);
-              },
-              child: const Icon(CupertinoIcons.add)),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: _buildBottomAppBar(),
-        ),
+    return Scaffold(
+      body: Padding(
+        padding: pagePadding,
+        child: SafeArea(child: _pages[_currentIndex]),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _onAddMedicine(context);
+          },
+          child: const Icon(CupertinoIcons.add)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: _buildBottomAppBar(),
     );
   }
 
   BottomAppBar _buildBottomAppBar() {
     return BottomAppBar(
-        elevation: 0,
         child: Container(
-          height: kBottomNavigationBarHeight,
-          color: Colors.yellow,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              CupertinoButton(
-                  child: Icon(
-                    CupertinoIcons.checkmark,
-                    color: _currentIndex == 0
-                        ? CustomColors.primaryColor
-                        : Colors.grey[400],
-                  ),
-                  onPressed: () => _onSelectPage(0)),
-              CupertinoButton(
-                  child: Icon(
-                    CupertinoIcons.text_badge_checkmark,
-                    color: _currentIndex == 1
-                        ? CustomColors.primaryColor
-                        : Colors.grey[400],
-                  ),
-                  onPressed: () => _onSelectPage(1))
-            ],
-          ),
-        ));
+      height: kBottomNavigationBarHeight,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CupertinoButton(
+              child: Icon(
+                CupertinoIcons.checkmark,
+                color: _currentIndex == 0
+                    ? CustomColors.primaryColor
+                    : Colors.grey[400],
+              ),
+              onPressed: () => _onSelectPage(0)),
+          CupertinoButton(
+              child: Icon(
+                CupertinoIcons.text_badge_checkmark,
+                color: _currentIndex == 1
+                    ? CustomColors.primaryColor
+                    : Colors.grey[400],
+              ),
+              onPressed: () => _onSelectPage(1))
+        ],
+      ),
+    ));
   }
 
   void _onSelectPage(int pageIndex) {
--- flutter_application_heetae/lib/pages/today/today_page.dart
+++ flutter_application_heetae/lib/pages/today/today_page.dart
@@ -1,12 +1,154 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_heetae/components/custom_constant.dart';
 
 class TodayPage extends StatelessWidget {
-  const TodayPage({super.key});
+  TodayPage({super.key});
+
+  final list = [
+    '약1',
+    '약이름',
+    '약이름 테스트',
+    '약이름약이름약이름약이름약이름약이름약이름',
+    '약이름약이름약이름약이름약이름약이름약이름',
+    '약이름약이름약이름약이름약이름약이름약이름',
+    '약이름약이름약이름약이름약이름약이름약이름',
+    '약이름약이름약이름약이름약이름약이름약이름',
+    '약이름약이름약이름약이름약이름약이름약이름',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '오늘 약 복용할 약은?',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        const SizedBox(height: regularSpace),
+        const Divider(
+          // 구분선
+          height: 1,
+          thickness: 2.0,
+        ),
+        Expanded(
+            // ListView.builder() : list 기반으로 view 가 그려진다.
+            // ListView.separated(separatorBuilder) : itemBuilder 사이사이마다 separatorBuilder 의 위젯을 추가
+            child: ListView.separated(
+          padding: const EdgeInsets.symmetric(vertical: smallSpace),
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return MedicineListTitle(
+              name: list[index],
+            );
+          },
+          // itemBuilder 사이 구분 하기 위한 위젯(여백을 넣어서 구분하게 만들었다.)
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider(
+              height: regularSpace,
+            );
+          },
+        )),
+        const Divider(
+          // 구분선
+          height: 1,
+          thickness: 2.0,
+        ),
+      ],
     );
+  }
+}
+
+class MedicineListTitle extends StatelessWidget {
+  const MedicineListTitle({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme.bodyText2;
+
+    return Row(
+      children: [
+        CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {},
+          child: const CircleAvatar(
+            radius: 40,
+          ),
+        ),
+        const SizedBox(
+          width: smallSpace,
+        ),
+        Expanded(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(
+              '8:30',
+              style: textStyle,
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Wrap(
+              crossAxisAlignment: WrapCrossAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: textStyle,
+                ),
+                TileActionButton(
+                  onTap: () {},
+                  title: '지금',
+                ),
+                Text(
+                  '|',
+                  style: textStyle,
+                ),
+                TileActionButton(
+                  onTap: () {},
+                  title: '아까',
+                ),
+                Text(
+                  '먹었어요',
+                  style: textStyle,
+                ),
+              ],
+            )
+          ]),
+        ),
+        CupertinoButton(
+            onPressed: () {},
+            child: const Icon(CupertinoIcons.ellipsis_vertical))
+      ],
+    );
+  }
+}
+
+class TileActionButton extends StatelessWidget {
+  const TileActionButton({
+    Key? key,
+    required this.onTap,
+    required this.title,
+  }) : super(key: key);
+
+  final VoidCallback onTap;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    final buttonTextStyle = Theme.of(context)
+        .textTheme
+        .bodyText2
+        ?.copyWith(fontWeight: FontWeight.w500);
+    return GestureDetector(
+        onTap: onTap,
+        child: Text(
+          title,
+          style: buttonTextStyle,
+        ));
   }
 }
