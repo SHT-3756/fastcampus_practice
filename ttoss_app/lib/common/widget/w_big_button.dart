21f8b6a3746caadc82747202ee2937af137cca28
SHT-3756 <gkdlfndkekd@naver.com>
2023-10-10 02:13:36 +0900

    feat : 큰 버튼 공통 위젯 생성

---
+++ ttoss_app/lib/common/widget/w_big_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ttoss_app/common/common.dart';
+import 'package:ttoss_app/common/widget/w_arrow.dart';
+import 'package:ttoss_app/common/widget/w_rounded_container.dart';
+
+class BigButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const BigButton(this.text, {Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RoundedContainer(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          text.text.white.size(20).bold.make(),
+          const Arrow(),
+        ],
+      ),
+    );
+  }
+}
