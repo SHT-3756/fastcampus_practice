0c4710e1b87a2130522e882d46889fd46f8dd843
SHT-3756 <gkdlfndkekd@naver.com>
2023-10-14 21:14:11 +0900

    feat : 알림 모델 생성

---
+++ ttoss_app/lib/screen/notification/vo/vo_notification.dart
@@ -0,0 +1,9 @@
+import 'package:ttoss_app/screen/notification/vo/notification_type.dart';
+
+class Notification {
+  final NotificationType type;
+  final String description;
+  final DateTime time;
+
+  Notification(this.type, this.description, this.time);
+}
