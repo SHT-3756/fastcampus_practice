5d693d2fb57e7533c4b585f631050c66a642aa64
SHT-3756 <gkdlfndkekd@naver.com>
2023-10-17 02:32:30 +0900

    feat : main.dart - timeago 초기화 feat : w_notification_item.dart - 위젯 선언 및 timeago 선언

--- ttoss_app/lib/main.dart
+++ ttoss_app/lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app.dart';
 import 'common/data/preference/app_preferences.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 void main() async {
   final bindings = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: bindings); // flutterNativeSlash 패키지 안에서 기존의 네이티브 화면을 splash 를 보존하고 유지시켜준다.
   await EasyLocalization.ensureInitialized();
   await AppPreferences.init();
+  timeago.setLocaleMessages('ko', timeago.KoMessages());
 
   runApp(EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ko')],
