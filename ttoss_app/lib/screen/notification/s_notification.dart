e2e37cd693e3970c87e27ec8d67a917de56984e6
SHT-3756 <gkdlfndkekd@naver.com>
2023-10-17 02:19:51 +0900

    feat : 알림 스크린 더미데이터 적용

--- ttoss_app/lib/screen/notification/s_notification.dart
+++ ttoss_app/lib/screen/notification/s_notification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ttoss_app/screen/notification/notification_dummy.dart';
+import 'package:ttoss_app/screen/notification/w_notification_item.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -10,12 +12,20 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Text('알림'),
-        )
-      ],
+    return Material(
+      child: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            title: Text('알림'),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            (context, index) =>
+                NotificationItemWidget(notification: notificationDummies[index]),
+            childCount: notificationDummies.length,
+          ))
+        ],
+      ),
     );
   }
 }
